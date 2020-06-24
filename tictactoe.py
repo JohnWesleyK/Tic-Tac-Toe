430623bb8778270fe5c9e6bdf91f9a1a2d4c53b8
th3j9 <johnwesleygithub@gmail.com>
2020-06-24 18:31:48 +0100

    Version 1.0.2

--- tictactoe.py
+++ tictactoe.py
@@ -90,3 +90,20 @@ def player_choice(board):
 # returns a boolean True if they do want to play again
 def replay():
     pass
+
+# print('Welcome to Tic Tac Toe!')
+
+# while True:
+# Set the game up here
+# pass
+
+# while game_on:
+# Player 1 Turn
+
+
+# Player2's turn.
+
+# pass
+
+# if not replay():
+# break
